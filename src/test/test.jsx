f985b802be5b9d61b58140862009b4f37aee4170
gmercer <gmercer@stanford.edu>
2025-10-10 11:03:55 -0700

    Adding expand button, and attempt to request display mode to fullscreen.

--- src/test/test.jsx
+++ src/test/test.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useWidgetProps } from "../use-widget-props";
+import { useDisplayMode } from "../use-display-mode";
+
 
 const ExpandIcon = () => {
   return (
@@ -18,36 +20,59 @@ const ExpandIcon = () => {
 export function App() {
   const widgetProps = useWidgetProps() || {};
   const { title_text } = widgetProps;
+  const displayMode = useDisplayMode();
+  const maxHeight = "100vh";
 
   const helloAgain = async () => {
-    await window.openai?.callTool("test-tool", { "title_text": "hi again. :)" });
+    await window.openai.callTool("test-tool", { "title_text": "hi again. :)" });
   };
 
   return (
-    <div style={{
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      padding: '16px',
-      fontSize: '18px',
-      fontWeight: 'bold'
-    }}>
-      <div>{title_text || 'hi'}</div>
-      <button
-        onClick={helloAgain}
-        style={{
-          marginTop: '10px',
-          padding: '8px 16px',
-          fontSize: '14px',
-          cursor: 'pointer',
-          backgroundColor: 'green',
-          color: 'white',
-          border: 'none',
-          borderRadius: '4px'
-        }}
-      >
-        Say Hello Again
-      </button>
+    <div
+      className={`antialiased w-full relative bg-blue-300 overflow-hidden ${displayMode !== "fullscreen" ? "aspect-[640/480] sm:aspect-[640/400]" : ""
+        }`}
+      style={{
+        maxHeight,
+        height: displayMode === "fullscreen" ? maxHeight : undefined,
+      }}
+    >
+      {displayMode !== "fullscreen" && (
+        <div className="fixed end-3 z-20 top-3 aspect-square rounded-full p-2 bg-white/20 text-white backdrop-blur-lg">
+          <button
+            onClick={() => {
+              window.openai.requestDisplayMode({ mode: "fullscreen" });
+            }}
+          >
+            <ExpandIcon />
+          </button>
+        </div>
+      )}
+
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        padding: '16px',
+        fontSize: '18px',
+        fontWeight: 'bold'
+      }}>
+        <div>{title_text || 'hi'}</div>
+        <button
+          onClick={helloAgain}
+          style={{
+            marginTop: '10px',
+            padding: '8px 16px',
+            fontSize: '14px',
+            cursor: 'pointer',
+            backgroundColor: 'green',
+            color: 'white',
+            border: 'none',
+            borderRadius: '4px'
+          }}
+        >
+          Say Hello Again
+        </button>
+      </div>
     </div>
   );
 }
